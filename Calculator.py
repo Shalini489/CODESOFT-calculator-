958ccf3e4291139a889ee90392a0e623c159a098
Shalini489 <139011805+Shalini489@users.noreply.github.com>
2023-08-12 20:41:26 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# **************** build a simple calculator in python ************
+# 1. ADD
+# 2.SUBTRACT
+# 3.MULTIPLY
+# 4.DIVIDE
+print("Mini Calculator")
+a = int(input('please enter the first number:'))
+b = int(input('please enter the second number:'))
+print('thank you so much for entering two numbers:')
+
+print("press 1 for addition \npress 2 for subtraction\npress3 for multiplication \npress4 for division")
+
+
+
+# In[4]:
+
+
+choice = int(input("enter your choice from 1-4:"))
+
+if choice ==1:
+    print(a + b)
+elif choice ==2:
+    print(a - b)
+elif choice ==3:
+    print(a * b)
+elif choice ==4:
+    print(a / b)
+else:
+    print("Invalid input")
+
+
+# In[6]:
+
+
+choice = int(input("enter your choice from 1-4:"))
+
+if choice ==1:
+    print(a + b)
+elif choice ==2:
+    print(a - b)
+elif choice ==3:
+    print(a * b)
+elif choice ==4:
+    print(a / b)
+else:
+    print("Invalid input")
+
+
+# In[7]:
+
+
+choice = int(input("enter your choice from 1-4:"))
+
+if choice ==1:
+    print(a + b)
+elif choice ==2:
+    print(a - b)
+elif choice ==3:
+    print(a * b)
+elif choice ==4:
+    print(a / b)
+else:
+    print("Invalid input")
+
+
+# In[8]:
+
+
+choice = int(input("enter your choice from 1-4:"))
+
+if choice ==1:
+    print(a + b)
+elif choice ==2:
+    print(a - b)
+elif choice ==3:
+    print(a * b)
+elif choice ==4:
+    print(a / b)
+else:
+    print("Invalid input")
+
+
+# In[9]:
+
+
+choice = int(input("enter your choice from 1-4:"))
+
+if choice ==1:
+    print(a + b)
+elif choice ==2:
+    print(a - b)
+elif choice ==3:
+    print(a * b)
+elif choice ==4:
+    print(a / b)
+else:
+    print("Invalid input")
+
+
+
+# In[ ]:
+
+
+
+
